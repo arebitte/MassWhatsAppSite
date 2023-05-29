3a6141140b6a4d0af17d35c8ec051ece4e59b5d3
Andre Rebitte <rebitte@gmail.com>
2023-05-29 09:38:11 -0300

    versão estável com registro, QRcode e envio

--- Controllers/WhatsAppController.cs
+++ Controllers/WhatsAppController.cs
@@ -2,11 +2,14 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MassWhatsAppSite.Models;
 using RestSharp;
+using System;
 using System.Linq;
+using System.Threading;
 
 public class WhatsAppController : Controller
 {
     private readonly ILogger<WhatsAppController> _logger;
+    private static string apiKeyHidden;
 
     public WhatsAppController(ILogger<WhatsAppController> logger)
     {
@@ -37,14 +40,14 @@ public class WhatsAppController : Controller
 
         var phoneNumbers = msg.PhoneNumbers.Split('\n').Select(n => n.Trim()).ToList();
 
-        var client = new RestClient("http://localhost:3333/message/text?key=andre_pessoal_v2");
+        var client = new RestClient("http://localhost:3333/message/text?key=" + apiKeyHidden);
         var request = new RestRequest();
         request.Method = Method.Post;
         request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
 
         foreach (var phoneNumber in phoneNumbers)
         {
-            var newRequest = new RestRequest(); // Criar nova instância de RestRequest
+            var newRequest = new RestRequest();
             newRequest.Method = Method.Post;
             newRequest.AddHeader("Content-Type", "application/x-www-form-urlencoded");
 
@@ -62,7 +65,7 @@ public class WhatsAppController : Controller
                 _logger.LogError("Falha ao enviar mensagem para o número: {phoneNumber}, StatusCode: {statusCode}, ErrorMessage: {errorMessage}", phoneNumber, response.StatusCode, response.ErrorMessage);
             }
 
-            Thread.Sleep(5000); // Aguarda 5 segundos antes da próxima iteração
+            Thread.Sleep(9000);
         }
 
         TempData["Success"] = "Mensagens enviadas com sucesso para todos os números.";
@@ -71,17 +74,84 @@ public class WhatsAppController : Controller
     }
 
     [HttpGet]
-    public IActionResult RegisterPhoneNumber(string key)
+    public IActionResult RegisterPhoneNumber(string phoneNumber, string apiKey)
     {
+        apiKeyHidden = apiKey;
+
         var token = "RANDOM_STRING_HERE_RANDOM123"; // Substitua pela sua lógica para gerar um token aleatório
-        var url = $"http://localhost:3333/instance/init?token={token}&key={key}";
-        return Redirect(url);
+        var url = $"http://localhost:3333/instance/init?token={token}&key={apiKey}";
+
+        // Fazer o post na API
+        var register = new RestClient(url);
+        var request = new RestRequest();
+        request.Method = Method.Get;
+        // Configurar os parâmetros da requisição, se necessário
+        // ...
+
+        // Executar a requisição
+        var response = register.Execute(request);
+
+        if (response.IsSuccessful)
+        {
+            // A requisição foi bem-sucedida, definir a mensagem de sucesso
+            TempData["Success"] = "Telefone registrado com sucesso!";
+        }
+        else
+        {
+            // A requisição falhou, definir a mensagem de erro
+            TempData["Error"] = "Falha ao registrar o telefone. Por favor, tente novamente.";
+        }
+
+        return RedirectToAction("Index");
     }
 
     [HttpGet]
-    public IActionResult ScanQR(string key)
+    public IActionResult ScanQR()
     {
-        var url = $"http://localhost:3333/instance/scan-qr?key={key}";
-        return Redirect(url);
+        var url = $"http://localhost:3333/instance/qrbase64?key={apiKeyHidden}";
+
+        // Fazer o post na API
+        var scan = new RestClient(url);
+        var request = new RestRequest();
+        request.Method = Method.Get;
+        // Configurar os parâmetros da requisição, se necessário
+        // ...
+
+        // Executar a requisição
+        var response = scan.Execute(request);
+
+        if (response.IsSuccessful)
+        {
+            // A requisição foi bem-sucedida, definir a mensagem de sucesso
+            TempData["Success"] = "Leia o QRCode com WhatsApp";
+
+            // Obter a imagem do QR Code retornada pela API
+            string qrCodeImage = response.Content;
+
+            // Converter a imagem para base64
+            //string qrCodeBase64 = "";//Convert.ToBase64String(qrCodeImage);
+            int index = qrCodeImage.IndexOf(";base64,");
+
+            string qrCodeImage_esq = qrCodeImage.Substring(index+8);
+
+            int index2 = qrCodeImage_esq.IndexOf("}");
+
+            string qrCodeBase64 = qrCodeImage_esq.Substring(0,index2-1);
+
+            
+
+            //qrCodeBase64 = qrCodeBase64_SP;
+
+            // Armazenar a imagem em base64 no TempData para exibir no frontend
+            TempData["QRCodeBase64"] = qrCodeBase64;
+        }
+        else
+        {
+            // A requisição falhou, definir a mensagem de erro
+            TempData["Error"] = "Falha ao gerar QRCode. Por favor, tente novamente.";
+        }
+
+        return RedirectToAction("Index");
     }
+
 }
--- Models/WhatsAppMessage.cs
+++ Models/WhatsAppMessage.cs
@@ -1,10 +1,12 @@
 namespace MassWhatsAppSite.Models
 {
-    public class WhatsAppMessage
-    {
-        public string PhoneNumbers { get; set; }
-        public string Message { get; set; } = "variavel texto";
-    }
+
+public class WhatsAppMessage
+{
+    public string PhoneNumbers { get; set; }
+    public string Message { get; set; }
+    public string QRCodeUrl { get; set; } // Adicione esta propriedade
+}
 
 
 }
